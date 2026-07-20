c8d4de5247b0166f592fb3c7e4c51be301289bd3
woodsauce <littlered_09@hotmail.com>
2026-07-19 22:21:38 -0600

    Delete api directory

--- api/coinbase-oauth-disconnect.js
+++
@@ -1,6 +0,0 @@
-const { json, clearCookie, SESSION_COOKIE } = require('./_coinbaseSession');
-
-module.exports = async function handler(req, res) {
-  res.setHeader('Set-Cookie', clearCookie(SESSION_COOKIE));
-  json(res, 200, { ok: true, disconnected: true });
-};
